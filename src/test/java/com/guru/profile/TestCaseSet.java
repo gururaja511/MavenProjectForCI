5685ca66f5c2bc212406440a0515fbacb46cb200
“gururaja511” <gururaja511@gmail.com>
2018-07-14 15:55:28 +0530

    second commit

--- src/test/java/com/guru/profile/TestCaseSet.java
+++ src/test/java/com/guru/profile/TestCaseSet.java
@@ -8,6 +8,31 @@ public class TestCaseSet {
   @Test
   public void f() {
 	  System.out.println("test1");
+	  
+	  String str="ABCD,,,,,,EFGH,,,,,,HDEF,,,,,,,,DEF";
+	  String dest="";
+	int con=0;
+	  for(int i=0;i<str.length();i++)
+	  {
+		  
+		  if(!(str.charAt(i)==',')) {
+			  
+			  dest=dest+str.charAt(i);
+			  con=0;
+		  }else if((str.charAt(i)==',') && con==0) {
+			  dest=dest+str.charAt(i);
+			  con=1;
+		  }
+		  	  
+		  
+	  }
+	  
+	  System.out.println(dest);
+	  
+	  
+	  
+	  
+	  
   }
   @BeforeMethod
   public void beforeMethod() {
