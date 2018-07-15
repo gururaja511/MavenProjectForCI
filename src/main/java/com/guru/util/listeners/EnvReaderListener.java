aeea435d9249f34eb4407c938f891e91c3772a42
“gururaja511” <gururaja511@gmail.com>
2018-07-15 11:27:22 +0530

    added env variable

---
+++ src/main/java/com/guru/util/listeners/EnvReaderListener.java
@@ -0,0 +1,30 @@
+package com.guru.util.listeners;
+
+import java.util.Map;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+public class EnvReaderListener implements ISuiteListener {
+
+	@Override
+	public void onFinish(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ISuite suite) {
+		// TODO Auto-generated method stub
+		
+		Map<String, String> parameters = suite.getXmlSuite().getParameters();
+		for (Map.Entry<String, String> parameter : parameters.entrySet()) {
+            String env = System.getenv(parameter.getKey());
+            if (env != null && ! env.trim().isEmpty()) {
+                parameter.setValue(env);
+            }
+        }
+		
+	}
+
+}
