7dbbb1331e283ad2f1d6f4ce18c94de4a7ed890c
“gururaja511” <gururaja511@gmail.com>
2018-07-15 12:35:18 +0530

    added isReports flage to handle form testng

--- src/test/java/com/guru/profile/TestCaseSet.java
+++ src/test/java/com/guru/profile/TestCaseSet.java
@@ -7,45 +7,46 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterMethod;
 
 public class TestCaseSet {
-  @Test
-  public void f() {
-	  System.out.println("test1");
-	  
-	  String str="ABCD,,,,,,EFGH,,,,,,HDEF,,,,,,,,DEF";
-	  String dest="";
-	int con=0;
-	  for(int i=0;i<str.length();i++)
-	  {
-		  
-		  if(!(str.charAt(i)==',')) {
-			  
-			  dest=dest+str.charAt(i);
-			  con=0;
-		  }else if((str.charAt(i)==',') && con==0) {
-			  dest=dest+str.charAt(i);
-			  con=1;
-		  }
-		  	  
-		  
-	  }
-	  
-	  System.out.println(dest);
-	  
-	  
-	  
-	  
-	  
-  }
-  @Parameters("env")
-  @BeforeMethod
-  public void beforeMethod(@Optional String env) {
-	  System.out.println("test before method");
-	  System.out.println("--------- env starte is "+env+"--------");
-  }
 
-  @AfterMethod
-  public void afterMethod() {
-	  System.out.println("test after method");
-  }
+	@Parameters("isReports")
+	@Test
+	public void f(@Optional String isReports) {
+		System.out.println("test1 started with isReports as "+isReports);
+		String str="ABCD,,,,,,EFGH,,,,,,HDEF,,,,,,,,DEF";
+		String dest="";
+		int con=0;
+		for(int i=0;i<str.length();i++)
+		{
+
+			if(!(str.charAt(i)==',')) {
+
+				dest=dest+str.charAt(i);
+				con=0;
+			}else if((str.charAt(i)==',') && con==0) {
+				dest=dest+str.charAt(i);
+				con=1;
+			}
+
+
+		}
+
+		System.out.println(dest);
+
+
+
+
+
+	}
+	@Parameters("env")
+	@BeforeMethod
+	public void beforeMethod(@Optional String env) {
+		System.out.println("test before method");
+		System.out.println("--------- env starte is "+env+"--------");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("test after method");
+	}
 
 }
